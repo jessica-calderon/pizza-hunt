37f5d44ee4cd4eda1184473dca4e2017f0f014fa
jessica-calderon <calderonjessica13@yahoo.com>
2022-08-25 02:12:55 -0500

    data-validation

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,13 +1,17 @@
-const dateFormat = require('../utils/dateFormat');
+const dateFormat = require("../utils/dateFormat");
 const { Schema, model } = require("mongoose");
 
 const PizzaSchema = new Schema(
   {
     pizzaName: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdBy: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdAt: {
       type: Date,
@@ -16,6 +20,8 @@ const PizzaSchema = new Schema(
     },
     size: {
       type: String,
+      required: true,
+      enum: ["Personal", "Small", "Medium", "Large", "Extra Large"],
       default: "Large",
     },
     toppings: [],
@@ -29,14 +35,17 @@ const PizzaSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
-      getters: true
+      getters: true,
     },
   }
 );
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
 const Pizza = model("Pizza", PizzaSchema);
 
